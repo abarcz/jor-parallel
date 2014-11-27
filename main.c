aa7cfd315ef2658dcc8f206e25fa67131a8d0beb
Aleksy Barcz <abarcz@gmail.com>
2014-11-27 20:19:17 +0100

    added number of tests to command line args

--- main.c
+++ main.c
@@ -54,80 +54,85 @@ void swap(double** a, double** b)
 
 int main(int argc, char* argv[])
 {
-	if (argc != 2) {
-		printf("program must be called with matrix dimension as the only argument\n");
+	if (argc != 3) {
+		printf("program must be called with arguments: matrix_dimension tests_number\n");
 		exit(1);
 	}
 	const int M = atoi(argv[1]);
 	printf("Using matrix dimension: %d\n", M);
+	const int tests = atoi(argv[2]);
 
 	// always use the same seed to get the same matrices during tests
 	srand(0);
 
 	const bool verbose = false;
-
-	const DataSet dataset = generate_dataset(M);
-
-	double* last_x = aligned_vector(M, false);
-	double* x = aligned_vector(M, false);
-	for (int i = 0; i < M; i++) {
-		x[i] = 1;
-	}
-
 	const double min_diff = 0.000001;
 	const double alpha = 1.01;
-	int iterations = 0;
 
-	// solve Ax = b
-	const double start_time = omp_get_wtime();
+	for (int k = 0; k < tests; k++) {
 
-	double sum = 0;
-	int j = 0;
-	int i = 0;
-	const double* A = dataset.A;
-	const double* b = dataset.b;
-	#pragma omp parallel shared(last_x, x) private(i, j, sum)
-	while (array_diff(x, last_x, M) > min_diff) {
-		swap(&last_x, &x);
+		const DataSet dataset = generate_dataset(M);
 
-		// A, M, alpha and b are constant, so they cannot be declared as shared
-		#pragma omp for schedule(dynamic)
-		for (i = 0; i < M; i++) {
-			sum = 0;
+		double* last_x = aligned_vector(M, false);
+		double* x = aligned_vector(M, false);
+		for (int i = 0; i < M; i++) {
+			x[i] = 1;
+		}
 
-			//#pragma omp target map(to: A, last_x) map(from: sum)a
-			#pragma omp simd reduction(+: sum) aligned(A, last_x: 16) linear(j)
-			for (j = 0; j < M; j++) {
-				sum += A[i * M + j] * last_x[j];
+		int iterations = 0;
+
+		// solve Ax = b
+		const double start_time = omp_get_wtime();
+
+		double sum = 0;
+		int j = 0;
+		int i = 0;
+		const double* A = dataset.A;
+		const double* b = dataset.b;
+		//#pragma omp parallel shared(last_x, x) private(i, j, sum)
+		while (array_diff(x, last_x, M) > min_diff) {
+			swap(&last_x, &x);
+
+			// A, M, alpha and b are constant, so they cannot be declared as shared
+			//#pragma omp for schedule(dynamic)
+			for (i = 0; i < M; i++) {
+				sum = 0;
+
+				//#pragma omp target map(to: A, last_x) map(from: sum)a
+				#pragma omp simd reduction(+: sum) aligned(A, last_x: 16) linear(j)
+				for (j = 0; j < M; j++) {
+					sum += A[i * M + j] * last_x[j];
+				}
+
+				sum -= A[i * M + i] * last_x[i];	// opt: outside the loop for sse optimizer
+				x[i] = (1 - alpha) * last_x[i] + alpha * (b[i] - sum) / A[i * M + i];
 			}
-
-			sum -= A[i * M + i] * last_x[i];	// opt: outside the loop for sse optimizer
-			x[i] = (1 - alpha) * last_x[i] + alpha * (b[i] - sum) / A[i * M + i];
+			iterations++;
 		}
-		iterations++;
-	}
-	const double end_time = omp_get_wtime();
-	const double seconds_spent = end_time - start_time;
+		const double end_time = omp_get_wtime();
+		const double seconds_spent = end_time - start_time;
 
-	if (verbose) {
-		printf("diff: %f\n", array_diff(x, last_x, M));
-		printf("x: ");
-		print_array(x, M);
-		printf("expected_x: ");
-		print_array(dataset.x, M);
-	}
-	double* bx = aligned_vector(M, false);
-	for (int i = 0; i < M; i++) {
-		for (int j = 0; j < M; j++) {
-			bx[i] += A[i * M + j] * x[j];
+		if (verbose) {
+			printf("diff: %f\n", array_diff(x, last_x, M));
+			printf("x: ");
+			print_array(x, M);
+			printf("expected_x: ");
+			print_array(dataset.x, M);
 		}
+		double* bx = aligned_vector(M, false);
+		for (int i = 0; i < M; i++) {
+			for (int j = 0; j < M; j++) {
+				bx[i] += A[i * M + j] * x[j];
+			}
+		}
+		if (verbose) {
+			printf("resulting b: ");
+			print_array(bx, M);
+		}
+		printf("RMSE: %0.10f\n", rmse(bx, b, M));
+		printf("iterations: %d\nseconds: %0.10f\n", iterations, seconds_spent);
+
 	}
-	if (verbose) {
-		printf("resulting b: ");
-		print_array(bx, M);
-	}
-	printf("RMSE: %0.10f\n", rmse(bx, b, M));
-	printf("iterations: %d\nseconds: %0.10f\n", iterations, seconds_spent);
 
 	return 0;
 }
